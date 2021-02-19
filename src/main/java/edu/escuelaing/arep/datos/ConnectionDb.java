573be9d5ff9392b742d2688765e57c1566c598ae
jsr25 <ramosjuans@live.com.ar>
2021-02-19 11:06:49 -0500

    modificacion index

--- src/main/java/edu/escuelaing/arep/datos/ConnectionDb.java
+++ src/main/java/edu/escuelaing/arep/datos/ConnectionDb.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 
 public class ConnectionDb {
     private String url = "jdbc:postgresql://ec2-54-198-73-79.compute-1.amazonaws.com:5432/d26bqfsllrt7l0?sslmode=require";
-    //private String driver="org.postgresql.Driver";
     private String usuario = "hnkjetvnwtrnil";
     private String contraseña = "69c5371fb42c6036ca4761c18369e7582d3af4343a41a44cb4d06eef747717cc";
 
@@ -16,7 +15,6 @@ public class ConnectionDb {
 
     public ConnectionDb() {
         try {
-            //Class.forName(driver);
             DriverManager.registerDriver(new org.postgresql.Driver());
             while (connect == null) {
                 connect = DriverManager.getConnection(url, usuario, contraseña);
